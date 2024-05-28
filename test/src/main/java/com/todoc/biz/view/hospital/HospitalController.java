8b6fb274b4d8e8b7ef9ba3c8dc66ea0f35c4d312
wkdmsdlek <wkdms1213@gmail.com>
2024-05-28 11:45:57 +0900

    공통수정

---
+++ test/src/main/java/com/todoc/biz/view/hospital/HospitalController.java
@@ -0,0 +1,41 @@
+package com.todoc.biz.view.hospital;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.todoc.biz.hospital.HospitalService;
+import com.todoc.biz.hospital.HospitalVO;
+
+@Controller
+@RequestMapping("/test2")
+public class HospitalController {
+	@Autowired
+	private HospitalService hospitalService;
+	
+	//병원목록
+	@RequestMapping("/getHospitalList.do")
+	public String getHospitalList(HospitalVO vo, Model model) {
+		System.out.println(">> 병원 목록");
+		
+		List<HospitalVO> hospitalList = hospitalService.getHospitalList(vo);
+		
+		model.addAttribute("hospitalList", hospitalList);
+		
+		return "hospital/getHospitalList";
+	}
+
+	//병원상세
+	public String getHospital(HospitalVO vo, Model model) {
+		System.out.println(">> 병원 상세");
+		
+		HospitalVO hospital = hospitalService.getHospital(vo);
+		
+		model.addAttribute("hospital", hospital);
+		
+		return "hospital/getHospital";
+	}
+}
