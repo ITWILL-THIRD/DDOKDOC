45e8ecc79c7c7948c7b693d818050d2e94724ef3
minji-git <kmjzz8168@gmail.com>
2024-06-12 09:59:28 +0900

    TimeMapper 파라미터sunLunchOff 추가 - mj

--- test/src/main/java/com/todoc/hospital/dao/TimeMapper.java
+++ test/src/main/java/com/todoc/hospital/dao/TimeMapper.java
@@ -7,24 +7,25 @@ import org.apache.ibatis.annotations.Update;
 import com.todoc.hospital.HospitalVO;
 
 public interface TimeMapper {
-	@Insert("INSERT INTO HOSTIME (HOSIDX, OPENTIME, CLOSETIME, LUNCHTIME, ENDLUNCHTIME, SATOPENTIME, SATCLOSETIME, SATLUNCHTIME, SATENDLUNCHTIME"
-			+ ", SUNOPENTIME, SUNCLOSETIME, SUNLUNCHTIME, SUNENDLUNCHTIME, LUNCHOFF, SATLUNCHOFF, SUNDAYOFF)"
-		+ "VALUES((SELECT MAX(NVL(HOSIDX, 0)) + 1 FROM HOSTIME), TO_DATE('1970-01-01 ' || #{openTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{closeTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", TO_DATE('1970-01-01 ' || #{lunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{endLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", TO_DATE('1970-01-01 ' || #{satOpenTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{satCloseTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", TO_DATE('1970-01-01 ' || #{satLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{satEndLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", TO_DATE('1970-01-01 ' || #{sunOpenTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{sunCloseTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", TO_DATE('1970-01-01 ' || #{sunLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{sunEndLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
-		+ ", #{lunchOff}, #{satLunchOff}, #{sunDayOff})")
-	int insertTime(HospitalVO vo
-		, @Param("openTimeStr")String openTimeStr, @Param("closeTimeStr")String closeTimeStr
-		, @Param("lunchTimeStr")String lunchTimeStr, @Param("endLunchTimeStr")String endLunchTimeStr
-		, @Param("satOpenTimeStr")String satOpenTimeStr, @Param("satCloseTimeStr")String satCloseTimeStr
-		, @Param("satLunchTimeStr")String satLunchTimeStr, @Param("satEndLunchTimeStr")String satEndLunchTimeStr
-		, @Param("sunOpenTimeStr")String sunOpenTimeStr, @Param("sunCloseTimeStr")String sunCloseTimeStr
-		, @Param("sunLunchTimeStr")String sunLunchTimeStr, @Param("sunEndLunchTimeStr")String sunEndLunchTimeStr
-		, @Param("lunchOff")String lunchOff, @Param("satLunchOff")String satLunchOff, @Param("sunDayOff")String sunDayOff
-		);
+	 @Insert("INSERT INTO HOSTIME (HOSIDX, OPENTIME, CLOSETIME, LUNCHTIME, ENDLUNCHTIME, SATOPENTIME, SATCLOSETIME, SATLUNCHTIME, SATENDLUNCHTIME"
+			+ ", SUNOPENTIME, SUNCLOSETIME, SUNLUNCHTIME, SUNENDLUNCHTIME, LUNCHOFF, SATLUNCHOFF, SUNDAYOFF, SUNLUNCHOFF)"
+			+ "VALUES((SELECT MAX(NVL(HOSIDX, 0)) + 1 FROM HOSTIME), TO_DATE('1970-01-01 ' || #{openTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{closeTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", TO_DATE('1970-01-01 ' || #{lunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{endLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", TO_DATE('1970-01-01 ' || #{satOpenTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{satCloseTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", TO_DATE('1970-01-01 ' || #{satLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{satEndLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", TO_DATE('1970-01-01 ' || #{sunOpenTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{sunCloseTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", TO_DATE('1970-01-01 ' || #{sunLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('1970-01-01 ' || #{sunEndLunchTimeStr}, 'YYYY-MM-DD HH24:MI:SS')"
+			+ ", COALESCE(#{lunchOff}, 'N'), COALESCE(#{satLunchOff}, 'N'), COALESCE(#{sunDayOff}, 'N'), COALESCE(#{sunLunchOff}, 'N'))")
+	 int insertTime(HospitalVO vo
+			 , @Param("openTimeStr")String openTimeStr, @Param("closeTimeStr")String closeTimeStr
+			 , @Param("lunchTimeStr")String lunchTimeStr, @Param("endLunchTimeStr")String endLunchTimeStr
+			 , @Param("satOpenTimeStr")String satOpenTimeStr, @Param("satCloseTimeStr")String satCloseTimeStr
+			 , @Param("satLunchTimeStr")String satLunchTimeStr, @Param("satEndLunchTimeStr")String satEndLunchTimeStr
+			 , @Param("sunOpenTimeStr")String sunOpenTimeStr, @Param("sunCloseTimeStr")String sunCloseTimeStr
+			 , @Param("sunLunchTimeStr")String sunLunchTimeStr, @Param("sunEndLunchTimeStr")String sunEndLunchTimeStr
+			 , @Param("lunchOff")String lunchOff, @Param("satLunchOff")String satLunchOff
+			 , @Param("sunDayOff")String sunDayOff, @Param("sunLunchOff")String sunLunchOff
+	);
 
 	
 	 @Update("UPDATE HOSTIME " +
