d9b0c2d444714f2960958a27178b8ea81d971878
minji-git <kmjzz8168@gmail.com>
2024-06-21 12:24:35 +0900

    Map import - mj

--- test/src/main/java/com/todoc/hospital/impl/HospitalServiceImpl.java
+++ test/src/main/java/com/todoc/hospital/impl/HospitalServiceImpl.java
@@ -1,7 +1,9 @@
 package com.todoc.hospital.impl;
 
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
